184bdebf14a21549d608db62219ec5e77b6d7250
coolwhite520 <81929154@qq.com>
2022-02-23 16:39:50 +0800

    ss

--- converters/converter.py
+++ converters/converter.py
@@ -3,29 +3,47 @@ import subprocess
 import os
 
 
-def p2d(src_file):
+def p2dx(src_file):
     portion = os.path.splitext(src_file)
     if len(portion) > 1:
         cv = Converter(src_file)
-        output_file = portion[0] + ".docx"
-        cv.convert(output_file)
+        output_file_docx = portion[0] + ".docx"
+        cv.convert(output_file_docx)
         cv.close()
+        # 将docx转doc
+        dx2d(output_file_docx)
+        # 删除中间的docx
+        os.unlink(output_file_docx)
+        output_file_dox = portion[0] + ".doc"
+        # 将doc转docx
+        d2dx(output_file_dox)
+        # 删除中间的doc
+        os.unlink(output_file_dox)
     else:
         raise IndexError("文件名解析错误")
 
 
-def d2d(src_file):
-    cmd = 'libreoffice  --invisible --convert-to docx {0}'.format(src_file)
+def dx2d(src_file):
+    output_dir = os.path.dirname(src_file)
+    cmd = 'libreoffice  --invisible --convert-to doc "{0}" --outdir "{1}"'.format(src_file, output_dir)
     subprocess.call(cmd, shell=True)
 
 
-def p2p(src_file):
-    cmd = 'libreoffice  --invisible --convert-to pptx {0}'.format(src_file)
+def d2dx(src_file):
+    output_dir = os.path.dirname(src_file)
+    cmd = 'libreoffice  --invisible --convert-to docx "{0}" --outdir "{1}"'.format(src_file, output_dir)
     subprocess.call(cmd, shell=True)
 
 
-def x2x(src_file):
-    cmd = 'libreoffice  --invisible --convert-to xlsx {0}'.format(src_file)
+def p2px(src_file):
+    output_dir = os.path.dirname(src_file)
+    cmd = 'libreoffice  --invisible --convert-to pptx "{0}" --outdir "{1}"'.format(src_file, output_dir)
+    subprocess.call(cmd, shell=True)
+
+
+def x2xx(src_file):
+    output_dir = os.path.dirname(src_file)
+    cmd = 'libreoffice  --invisible --convert-to xlsx "{0}" --outdir "{1}"'.format(src_file, output_dir)
     subprocess.call(cmd, shell=True)
 
 
@@ -36,11 +54,11 @@ class Converters(object):
         self.src_file = src_file
 
     def convert(self):
-        if self.convert_type == 'p2d':
-            p2d(self.src_file)
-        elif self.convert_type == 'd2d':
-            d2d(self.src_file)
-        elif self.convert_type == 'p2p':
-            p2p(self.src_file)
-        elif self.convert_type == 'x2x':
-            x2x(self.src_file)
+        if self.convert_type == 'p2dx':
+            p2dx(self.src_file)
+        elif self.convert_type == 'd2dx':
+            d2dx(self.src_file)
+        elif self.convert_type == 'p2px':
+            p2px(self.src_file)
+        elif self.convert_type == 'x2xx':
+            x2xx(self.src_file)
